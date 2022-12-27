f4138bc519d431ac085e987a3bc8398d4e82b71f
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-27 20:01:42 +0300

    Update Poo.php

--- Basis/Poo.php
+++ Basis/Poo.php
@@ -24,3 +24,6 @@ $person = new Person('John', 30);
 // Appel de la méthode greet() de l'objet $person
 echo $person->greet();
 
+//Ce code crée une classe Person avec deux propriétés publiques : $name et $age. Il y a également un constructeur qui permet de définir ces propriétés lors de la création d'une nouvelle instance de la classe. Enfin, il y a une méthode greet() qui retourne une chaîne de caractères contenant le nom et l'âge de la personne.
+
+//Lorsque le code est exécuté, il crée une nouvelle instance de la classe Person et appelle la méthode greet() pour afficher un message de salutation. Le résultat final affiché sera : "Bonjour, je m'appelle John et j'ai 30 ans."
