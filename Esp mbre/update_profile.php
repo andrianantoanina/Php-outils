4445d77fa86bf8c9f45f05d0ab53a7cbaa6f54f2
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-30 10:18:53 +0300

    Create update_profile.php

---
+++ Esp mbre/update_profile.php
@@ -0,0 +1,13 @@
+<?php
+  // Récupération des données du formulaire
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Connexion à la base de données et mise à jour des informations de l'utilisateur
+  $db = mysqli_connect('localhost', 'utilisateur', 'motdepasse', 'nomdelabasededonnées');
+  $query = "UPDATE utilisateurs SET email='$email', password='$password' WHERE username='$username'";
+  mysqli_query($db, $query);
+
+  // Redirection vers la page de profil
+  header('Location: profile.php');
+?>
