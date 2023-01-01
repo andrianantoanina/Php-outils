94240b94e98591243212ce22e0511c424ccda2de
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2023-01-01 19:21:57 +0300

    Update update_profile.php

--- Esp mbre/update_profile.php
+++ Esp mbre/update_profile.php
@@ -3,7 +3,7 @@
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  // Connexion à la base de données et mise à jour des informations de l'utilisateur
+  // Connexion à la base de données et mise à jour des informations de l'utilisateur.
   $db = mysqli_connect('localhost', 'utilisateur', 'motdepasse', 'nomdelabasededonnées');
   $query = "UPDATE utilisateurs SET email='$email', password='$password' WHERE username='$username'";
   mysqli_query($db, $query);
